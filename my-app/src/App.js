87c1fe411ceb6e6491cfc9a7c8e94bf3783a06b2
Jawad Chowdhury <jawadchy15@gmail.com>
2023-06-04 23:44:05 -0400

    Upload File
    Uploads files to firebase storage

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { useState } from "react";
+import React from 'react';
+import { storage } from "./firebase";
+import { ref, uploadBytes } from "firebase/storage";
+import { v4 } from "uuid";
+
+function App() {
+
+      const [fileUpload, setFileUpload] = useState(null);
+      const uploadFile = () => {
+        if (fileUpload == null) return;
+        const fileRef = ref(storage, `files/${fileUpload.name + v4()}`);
+        uploadBytes(fileRef, fileUpload).then(() => {
+          alert("File Uploaded");
+        })
+      }
+  return (
+    <div className="App">
+      <input type = "file" onChange={
+        (event) => {setFileUpload(event.target.files[0]);
+        }}/>
+      <button onClick={uploadFile}>Upload File</button>
+      </div>
+  );
+}
+
+export default App;
