f2ffe6dfedb853d954252eb16183a8865bc1baaa
Jawad Chowdhury <jawadchy15@gmail.com>
2023-06-10 20:18:16 -0400

    mvp done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,66 +1,214 @@
-// Import styles from App.css
-import './App.css';
-// Import the useState hook from the React library
-// This allows us to create state within functional components
-import React, { useState } from "react";
-// Import the storage object from the firebase library
+import React, { useState, useEffect } from "react";
 import { storage } from "./firebase";
-// Import the ref and uploadBytes functions from the storage module (Upload)
-import { ref, uploadBytes } from "firebase/storage";
-// Import the v4 function from the uuid library
-// This allows us to generate unique IDs for stored files
-import { v4 } from "uuid";
-// Import Bootstrap styles
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
+import * as pdfjsLib from "pdfjs-dist/build/pdf";
 
+// Initialize PDF.js with the worker
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 function App() {
-  // Use the useState hook to create a state variable called fileUpload, initially set to null
   const [fileUpload, setFileUpload] = useState(null);
-  // Define a function called uploadFile
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchQuery.trim() === "") {
+      setSearchResults([]);
+    } else {
+      searchFiles();
+    }
+  }, [searchQuery]);
+
   const uploadFile = () => {
-    // If fileUpload is null or undefined, return and do nothing
-    if (!fileUpload) return;
-    // Create a file reference in the storage object using the file's name and a v4 uuid
-    const fileRef = ref(storage, `files/${fileUpload.name + v4()}`);
-    // Upload the file to the storage object using the uploadBytes function and the file reference
-    uploadBytes(fileRef, fileUpload).then(() => {
-      // Alert the user that the file was successfully uploaded
-      alert("File Uploaded");
-    })
-  }
-  
-  // Render an input element that allows users to select a file to upload
-  // Add an event listener on the input element that sets the fileUpload state to the selected file
-  // Render a button that triggers the uploadFile function when clicked, only if a file has been selected
+    if (fileUpload == null) return;
+
+    const fileRef = ref(storage, `files/${fileUpload.name + uuidv4()}`);
+    uploadBytes(fileRef, fileUpload)
+      .then(() => {
+        alert("File Uploaded");
+        parsePDF(fileUpload);
+      })
+      .catch((error) => {
+        console.log("Upload Error:", error);
+      });
+  };
+
+  const parsePDF = (file) => {
+    const fileReader = new FileReader();
+    fileReader.onload = function () {
+      const typedArray = new Uint8Array(this.result);
+      const loadingTask = pdfjsLib.getDocument(typedArray);
+      loadingTask.promise.then(function (pdf) {
+        const numPages = pdf.numPages;
+        const getPageText = (page) =>
+          page.getTextContent().then(function (textContent) {
+            const pageText = textContent.items
+              .map((item) => item.str)
+              .join(" ");
+            return pageText;
+          });
+
+        const getPagePromises = Array.from(
+          { length: numPages },
+          (_, i) => i + 1
+        ).map((pageNum) => pdf.getPage(pageNum).then(getPageText));
+
+        Promise.all(getPagePromises)
+          .then((pageTexts) => {
+            const parsedText = pageTexts.join("\n");
+            storeParsedText(parsedText, file);
+          })
+          .catch((error) => {
+            console.log("PDF Parsing Error:", error);
+          });
+      });
+    };
+    fileReader.readAsArrayBuffer(file);
+  };
+
+  const storeParsedText = (parsedText, file) => {
+    const textFileRef = ref(storage, `files/${file.name}.txt`);
+    const textFileBlob = new Blob([parsedText], { type: "text/plain" });
+
+    uploadBytes(textFileRef, textFileBlob)
+      .then(() => {
+        console.log("Parsed text stored as text file");
+        searchFiles(); // Trigger search after storing parsed text
+      })
+      .catch((error) => {
+        console.log("Text File Upload Error:", error);
+      });
+  };
+
+  const openFileURL = (url) => {
+    window.open(url, "_blank");
+  };
+
+  const searchFiles = () => {
+    setIsLoading(true);
+    const filesRef = ref(storage, "files");
+    listAll(filesRef)
+      .then((res) => {
+        const searchPromises = res.items.map((item) =>
+          getDownloadURL(item).then((url) =>
+            fetchProxyFileContent(url, item.name)
+          )
+        );
+        Promise.all(searchPromises)
+          .then((results) => {
+            const filteredResults = results.filter((result) =>
+              result.content.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+            setSearchResults(filteredResults);
+            setIsLoading(false);
+          })
+          .catch((error) => {
+            console.log("Search Error:", error);
+            setIsLoading(false);
+          });
+      })
+      .catch((error) => {
+        console.log("Listing Files Error:", error);
+        setIsLoading(false);
+      });
+  };
+
+  const fetchProxyFileContent = (url, fileName) => {
+    const proxyUrl = `http://localhost:3001/fetch-file?url=${encodeURIComponent(
+      url
+    )}`;
+    return fetch(proxyUrl)
+      .then((response) => response.text())
+      .then((fileContent) => ({ name: fileName, content: fileContent, url })) // Include the 'url' property
+      .catch((error) => {
+        console.log("Fetch Error:", error);
+        return { name: fileName, content: null, url }; // Include the 'url' property
+      });
+  };
+
   return (
-    <div className="container mt-4">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="card">
-            <div className="card-body">
-              <h3 className="card-title mb-4">Upload your resume</h3>
-              <div className="form-group">
-                <input 
-                  type="file" 
-                  accept=".pdf, .docx, .doc, .txt"
-                  className="form-control-file" 
-                  onChange={
-                    (event) => {
-                      setFileUpload(event.target.files[0]);
-                    }
-                  }/>
-              </div>
-              <button 
-                className="btn btn-primary"
-                onClick={uploadFile}
-                disabled={!fileUpload}>
-                  Upload File
-              </button>
-            </div>
+    <div className="container">
+      <h1 className="text-center">Resume Storage</h1>
+      <div className="row justify-content-center mb-3">
+        <div className="col-6">
+          <div className="input-group">
+            <input
+              type="file"
+              className="form-control"
+              onChange={(event) => {
+                setFileUpload(event.target.files[0]);
+              }}
+            />
+            <button
+              className="btn btn-primary"
+              onClick={uploadFile}
+              disabled={isLoading}
+            >
+              Upload File
+            </button>
           </div>
+        </div>
+      </div>
+      <div className="row justify-content-center mb-3">
+        <div className="col-6">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search Resumes"
+              value={searchQuery}
+              onChange={(event) => {
+                setSearchQuery(event.target.value);
+              }}
+            />
+            {searchQuery.trim() === "" && (
+              <button
+                className="btn btn-secondary"
+                onClick={searchFiles}
+                disabled={isLoading}
+              >
+                Fetch All
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-6">
+          {isLoading ? (
+            <p className="text-center">Loading...</p>
+          ) : (
+            <ul className="list-group">
+              {searchResults.length === 0 ? (
+                <li className="list-group-item text-center">
+                  No results found
+                </li>
+              ) : (
+                searchResults.map((result) => (
+                  <li
+                    key={result.name}
+                    className="list-group-item d-flex justify-content-between align-items-center"
+                  >
+                    <span>{result.name}</span>
+                    <a
+                      href={result.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      View
+                    </a>
+                  </li>
+                ))
+              )}
+            </ul>
+          )}
         </div>
       </div>
     </div>
   );
 }
+
 export default App;
--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,23 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7_eDsPmH-iZVDvkszLK3df65IzBms3a8",
-  authDomain: "test-file-upload1.firebaseapp.com",
-  projectId: "test-file-upload1",
-  storageBucket: "test-file-upload1.appspot.com",
-  messagingSenderId: "634240679256",
-  appId: "1:634240679256:web:bdc211ab0828d3a0ebb92d",
-  measurementId: "G-WQ8NGFTELQ"
+  apiKey: "AIzaSyC2YE09rv4IGn9UGqKJtwjoUrKR6Il-C2o",
+  authDomain: "projblog-3b8b0.firebaseapp.com",
+  projectId: "projblog-3b8b0",
+  storageBucket: "projblog-3b8b0.appspot.com",
+  messagingSenderId: "164036988793",
+  appId: "1:164036988793:web:d3a82cb7a5db17ce5c29a8",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const storage = getStorage(app);
