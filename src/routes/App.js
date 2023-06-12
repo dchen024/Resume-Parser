bef3de1a6c68f4f39620c3a689d831a838f398ae
Jawad Chowdhury <jawadchy15@gmail.com>
2023-06-12 15:46:45 -0400

    added icons to buttons, and some more styling stuff

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,23 +1,23 @@
 import { Link } from "react-router-dom";
 import "./App.css";
+import customApplicantIcon from "../assets/custom-applicant-icon.png";
+import customRecruiterIcon from "../assets/custom-recruiter-icon.png";
 
 function App() {
   return (
-    
     <div className="container">
-      
-       <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet" />
-      <h1 className="title">
+      <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet" />
+      <h1 className="title custom-title">
         Resume Parser
       </h1>
       <div>
-      <h1 className="text-center"> 
-        <Link className="btn btn-primary btn-lg" to="/applicant" role="button">
-          Applicant
-        </Link>
-        <Link className="btn btn-danger btn-lg" to="/recruiter" role="button">
-          Recruiter
-        </Link>
+        <h1 className="text-center">
+          <Link className="btn btn-primary btn-lg" to="/applicant" role="button">
+            <img src={customApplicantIcon} alt="Applicant" /> Applicant
+          </Link>
+          <Link className="btn btn-danger btn-lg" to="/recruiter" role="button">
+            <img src={customRecruiterIcon} alt="Recruiter" /> Recruiter
+          </Link>
         </h1>
       </div>
     </div>
