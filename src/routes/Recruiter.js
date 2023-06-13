1be593aa9a39495582ede3dd6815bed53de06f10
Jawad Chowdhury <jawadchy15@gmail.com>
2023-06-12 20:14:15 -0400

    added delete feature in recruiter

--- src/routes/Recruiter.js
+++ src/routes/Recruiter.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { storage } from "../firebase"; // Import the Firebase storage module
-import { ref, listAll, getBlob, getDownloadURL } from "firebase/storage"; // Import the Firebase storage functions
+import { ref, listAll, getBlob, getDownloadURL, deleteObject } from "firebase/storage"; // Import the Firebase storage functions
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry"; // Import the PDF.js worker
 import * as pdfjsLib from "pdfjs-dist/build/pdf"; // Import the PDF.js library
 import { Link } from "react-router-dom"; // Import the React Router Link component
 import { AiFillHome } from 'react-icons/ai'; // Import the home icon from React Icons
 import "./App.css";
+import { FaTrash } from "react-icons/fa";
 
 // Initialize PDF.js with the worker
 pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
@@ -66,6 +67,23 @@ function Recruiter() {
     }
   }, [searchQuery, searchFiles]);
 
+  const handleDelete = async (name) => {
+    // Delete the PDF and TXT files from Firebase storage
+    const pdfRef = ref(storage, `pdf/${name}`);
+    const txtRef = ref(storage, `txt/${name.replace(/\.pdf$/, ".txt")}`);
+
+    await Promise.all([
+      deleteObject(pdfRef),
+      deleteObject(txtRef)
+    ]).catch((error) => {
+      console.error("Delete Error:", error);
+    });
+
+    setSearchResults((prevResults) =>
+      prevResults.filter((result) => result.name !== name)
+    );
+  };
+
   return (
     <div className="recruiter">
       <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet" />
@@ -123,13 +141,22 @@ function Recruiter() {
                     className="list-group-item d-flex justify-content-between align-items-center"
                   >
                     <span>{result.name}</span>
-                    <a
-                      href={result.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      View
-                    </a>
+                    <div className="d-flex justify-content-end">
+                      <a
+                        href={result.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn btn-info mr-2"
+                      >
+                        View
+                      </a>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => handleDelete(result.name)}
+                      >
+                        <FaTrash />
+                      </button>
+                    </div>
                   </li>
                 ))
               )}
